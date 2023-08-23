6b0171ee466cf5df079c4026e7933aac8194b80b
kunal98v <kunal98v@gmail.com>
2023-08-23 14:42:31 +0530

    my update

---
+++ index.js
@@ -0,0 +1,55 @@
+var name = document.querySelector("#name")
+var email = document.querySelector("#email")
+var pwd1 = document.querySelector("#pwdone")
+var pwd2 = document.querySelector("#pwdtwo")
+var btn = document.querySelector(".btn")
+var popup = document.querySelector(".popup")
+
+
+
+pwd2.addEventListener("change",(e)=>{
+
+if((pwd1.value != "" )&& (pwd2.value != "")){
+
+   if(pwd1.value === pwd2.value)
+   {
+        pwd1.classList.remove("red")
+        pwd1.classList.add("green")
+        pwd2.classList.remove("red")
+        pwd2.classList.add("green")
+        popup.innerHTML = ""
+
+   }
+        
+   else{
+          pwd2.classList.remove("green")
+          pwd2.classList.add("red")
+          pwd2.classList.remove("green")
+          pwd2.classList.add("red")
+          popup.innerHTML = "passwords do not match !"
+   }
+}
+})
+
+pwd1.addEventListener("change",(e)=>{
+
+     if((pwd1.value != "" )&& (pwd2.value != "")){
+     if(pwd2.value === pwd1.value)
+     {
+          pwd1.classList.remove("red")
+          pwd1.classList.add("green")
+          pwd2.classList.remove("red")
+          pwd2.classList.add("green")
+          popup.innerHTML = ""
+
+         
+     }
+     else{
+            pwd2.classList.remove("green")
+            pwd2.classList.add("red")
+            pwd2.classList.remove("green")
+            pwd2.classList.add("red")
+            popup.innerHTML = "passwords do not match"
+   }
+}
+  })
